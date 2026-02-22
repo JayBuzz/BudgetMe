aaf955daa71aaf9d1ae5ecf394fdf8550186e2cc
Joseph Thomas <josephathomas65@gmail.com>
2026-02-21 19:04:03 -0500

    Initial Commit

---
+++ Bbudget Me/Bbudget_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bbudget_MeApp.swift
+//  Bbudget Me
+//
+//  Created by Joseph Thomas on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Bbudget_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
